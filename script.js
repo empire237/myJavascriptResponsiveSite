42275f52368190c0040b9802111c2b1f516844ad
Divane Djoumetio <didjoumetio@gmail.com>
2023-10-19 03:26:06 +0100

    Adding a menu article to my Responsive site

--- script.js
+++ script.js
@@ -4,19 +4,14 @@ const sidebarCloseBtn = document.querySelector('.sidebar__close-icon');
 const sidebarMenuBtn = document.querySelector('.sidebar__menu-btn');
 const sidebarContainer = document.querySelector('.sidebar__container');
 
-console.log(sidebarContainer);
-console.log(sidebarMenuBtn);
-console.log(sidebarCloseBtn);
-
 
 
 // Event listener for Menu Button
 
 sidebarMenuBtn.addEventListener('click', function(){
         sidebarContainer.classList.toggle('sidebar-status');
-        console.log('outer');
         sidebarCloseBtn.addEventListener('click', function (){
             sidebarContainer.classList.add('sidebar-status');
-            console.log('inner');
         })
 });
+
